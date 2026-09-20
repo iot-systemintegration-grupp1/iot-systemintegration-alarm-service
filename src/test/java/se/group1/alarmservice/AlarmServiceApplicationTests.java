7274aff71b291820799e5ca3c22f6be70e527573
Kim Hedelin <kimhedelin@gmail.com>
2026-09-20 17:29:02 +0200

    fixed pom.xml to not seed database url when building

--- src/test/java/se/group1/alarmservice/AlarmServiceApplicationTests.java
+++ src/test/java/se/group1/alarmservice/AlarmServiceApplicationTests.java
@@ -3,7 +3,10 @@ package se.group1.alarmservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:alarm-service;MODE=MSSQLServer;DB_CLOSE_DELAY=-1",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class AlarmServiceApplicationTests {
 
     @Test
